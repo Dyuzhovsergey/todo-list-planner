aba073f66ea7a810a37e60fe99a7350004e5432d
Sergey <dyuzhovsergey@gmail.com>
2025-09-25 21:22:14 +0300

    add query for add task in db

--- pkg/db/task.go
+++ pkg/db/task.go
@@ -3,7 +3,20 @@ package db
 type Task struct {
 	ID      string `json:"id"`
 	Date    string `json:"date"`
-	TITLE   string `json:"title"`
-	COMMENT string `json:"comment"`
-	REPEAT  string `json:"repeat"`
+	Title   string `json:"title"`
+	Comment string `json:"comment"`
+	Repeat  string `json:"repeat"`
+}
+
+func AddTask(task *Task) (int64, error) {
+	var id int64
+	// определите запрос
+	query := `INSERT INTO scheduler (date, title, comment, repeat)
+	VALUES (?, ?, ?, ?)
+	`
+	res, err := db.Exec(query, task.Date, task.Title, task.Comment, task.Repeat)
+	if err == nil {
+		id, err = res.LastInsertId()
+	}
+	return id, err
 }
